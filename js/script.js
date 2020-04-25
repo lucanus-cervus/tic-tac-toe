28a2676bd55aa56eae55d28516c03ff0eb4b9755
Nolan <nolan.bigot@orange.fr>
2020-04-25 11:18:12 +0200

    Système vérification
    Système vérification gagnant, et case déjà colorié.

--- js/script.js
+++ js/script.js
@@ -1,27 +1,56 @@
-
 let id = 0;
 let ad = 0;
-let tableauOne = [];
-let tableauTwo = [];
+let gagner = 0;
 let test = {};
 let canvas = document.querySelector('#grilleCa')
 let decompte = document.getElementById("decompte");
 let playerOne = true;
-let positionBase = {
-    posX: 1000,
-    posY: 1000
-};
-let tableauFinale = [positionBase];
+let tableauTempo = []
+
+let tableauPosition = {
+    0:[0, 0],
+    1:[200, 0],
+    2:[400, 0],
+    3:[0, 200],
+    4:[200, 200],
+    5:[400, 200],
+    6:[0, 400],
+    7:[200, 400],
+    8:[400, 400]
+}
+let tableauResultat = [0, 0, 0,
+                       0, 0, 0,
+                       0, 0, 0];
+tableauRed = [
+    [1,1,1,0,0,0,0,0,0],
+    [0,0,0,1,1,1,0,0,0],
+    [0,0,0,0,0,0,1,1,1],
+    [1,0,0,1,0,0,1,0,0],
+    [0,1,0,0,1,0,0,1,0],
+    [0,0,1,0,0,1,0,0,1],
+    [1,0,0,0,1,0,0,0,1],
+    [0,0,1,0,1,0,1,0,0]
+]
+tableauBlue = [
+    [2,2,2,0,0,0,0,0,0],
+    [0,0,0,2,2,2,0,0,0],
+    [0,0,0,0,0,0,2,2,2],
+    [2,0,0,2,0,0,2,0,0],
+    [0,2,0,0,2,0,0,2,0],
+    [0,0,2,0,0,2,0,0,2],
+    [2,0,0,0,2,0,0,0,2],
+    [0,0,2,0,2,0,2,0,0]
+]
 
 document.getElementById("click", function(e){
     console.log(document.getElementById("colorChoice").options)
 });
 
 document.getElementById("startGame").addEventListener("click", function(e){
-    setTimeout(function(e){decompte.textContent = "3"}, 1000);
-    setTimeout(function(e){decompte.textContent = "2"}, 2000);
-    setTimeout(function(e){decompte.textContent = "1"}, 3000);
-    setTimeout(function(e){decompte.textContent = "Player 1"; game()}, 4000);
+    //setTimeout(function(e){decompte.textContent = "3"}, 1000);
+    //setTimeout(function(e){decompte.textContent = "2"}, 2000);
+    //setTimeout(function(e){decompte.textContent = "1"}, 3000);
+    setTimeout(function(e){decompte.textContent = "Player 1"; game()}, 10);
 });
 
 document.getElementById("rejouerGame").addEventListener("click", function(e){
@@ -39,190 +68,154 @@ function game(){
         id = 0;
     }
     document.getElementById("grilleCa").addEventListener("mousedown", function(e){
-            let encoche = true;
-            let tableauTest = [0, 0, 0, 0, 0, 0, 0, 0];
-            let tableauRed = [0, 0, 0, 0, 0, 0, 0, 0];
             const x = Math.floor(event.x);
             const y = Math.floor(event.y);
             resultX = x - (x % 200);
             resultY = y - (y % 200);
             let ctx = canvas.getContext("2d");
+            console.log(tableauPosition[0], tableauPosition[0].value);
             if(playerOne)
-            {
-                tableauFinale.map(function(e){
-                    if(e.posX == resultX && e.posY == resultY)
-                    {
-                        alert("case déjà colorée")
-                        playerOne = true;
-                        encoche = false;
-                    }
-                });
-                if(encoche == true)
+            {    
+                for(i = 0; i < 9; i++)
                 {
-                    ctx.fillStyle = "red";
-                    playerOne = false;
-                    positionRed = {
-                        posX: resultX,
-                        posY: resultY,
-                        color: "red"
-                    };
-                    tableauOne.push(positionRed);
-                    tableauFinale.push(positionRed);
-                    decompte.textContent = "Player 2";
-                    console.log(tableauOne);
-                    ctx.fillRect(resultX, resultY, 199, 199);
+                    if(tableauPosition[i][0] === resultX && tableauPosition[i][1] === resultY ){
+                        if(tableauResultat[i] == 0)
+                        {
+                            let xhr = new XMLHttpRequest;
+                            xhr.open("POST", "http://bigot.cefim.o2switch.site/test", true)
+                            xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+                            xhr.addEventListener("readystatechange", function(e){
+                                console.log(xhr.responseText);
+                            });
+                            xhr.send("resultX=" + resultX + "&resultY=" + resultY + "&color=red")
+                            tableauResultat[i] = 1
+                            ctx.fillStyle = "red"
+                            ctx.fillRect(resultX, resultY, 199, 199)
+                            playerOne = false;
+                            for(k = 0; k < 9; k++)
+                            {
+                                tableauTempo[k] = tableauResultat[k];
+                            }
+                            for(a = 0; a < 9; a++)
+                            {
+                                if(tableauResultat[a] == 2)
+                                {
+                                    tableauResultat[a] = 0;
+                                }
+                            }
+                            console.log(tableauResultat)
+                            for(o = 0; o < 8; o++ )
+                            {
+                                for(i = 0; i < 9; i++)
+                                { 
+                                    if(tableauRed[o][i] === tableauResultat[i])
+                                    {
+                                        gagner++;
+                                        if(gagner == 8)
+                                        {
+                                            console.log("c'est gagné");
+                                        } 
+                                    }
+                                    else
+                                    {
+                                        gagner = 0;
+                                        break;
+                                    }
+                                }
+                            }
+                            for(k = 0; k < 9; k++)
+                            {
+                                tableauResultat[k] = tableauTempo[k]
+                            }
+                        }
+                        else
+                        {
+                            console.log("case déjà colorié connard")
+                        }
+                        break;
+                    }
                 }
-                tableauOne.map(function(e){
-                    if(e.posX == 0 && e.posY == 0)
-                    {
-                        tableauRed[0] = tableauRed[0] + 1
-                        tableauRed[3] = tableauRed[3] + 1
-                        tableauRed[5] = tableauRed[5] + 1
-                    }
-                    else if(e.posX == 0 && e.posY == 200)
-                    {
-                        tableauRed[0] = tableauRed[0] + 1
-                        tableauRed[6] = tableauRed[6] + 1
-                    }
-                    else if(e.posX == 0 && e.posY == 400)
-                    {
-                        tableauRed[0] = tableauRed[0] + 1
-                        tableauRed[4] = tableauRed[4] + 1
-                        tableauRed[7] = tableauRed[7] + 1
-                    }
-                    else if(e.posX == 200 && e.posY == 0)
-                    {
-                        tableauRed[1] = tableauRed[1] + 1
-                        tableauRed[5] = tableauRed[5] + 1
-                    }
-                    else if(e.posX == 200 && e.posY == 200)
-                    {
-                        tableauRed[1] = tableauRed[1] + 1
-                        tableauRed[3] = tableauRed[3] + 1
-                        tableauRed[4] = tableauRed[4] + 1
-                        tableauRed[6] = tableauRed[6] + 1
-                    }
-                    else if(e.posX == 200 && e.posY == 400)
-                    {
-                        tableauRed[1] = tableauRed[1] + 1
-                        tableauRed[7] = tableauRed[7] + 1
-                    }
-                    else if(e.posX == 400 && e.posY == 0)
-                    {
-                        tableauRed[2] = tableauRed[2] + 1
-                        tableauRed[5] = tableauRed[5] + 1
-                    }
-                    else if(e.posX == 400 && e.posY == 200)
-                    {
-                        tableauRed[6] = tableauRed[6] + 1
-                        tableauRed[2] = tableauRed[2] + 1
-                    }
-                    else if(e.posX == 400 && e.posY == 400)
-                    {
-                        tableauRed[2] = tableauRed[2] + 1
-                        tableauRed[3] = tableauRed[3] + 1
-                        tableauRed[4] = tableauRed[4] + 1
-                        tableauRed[7] = tableauRed[7] + 1
-                    }
-                })
-            }
+            }   
             else
             {
-                tableauFinale.map(function(e){
-                    if(e.posX == resultX && e.posY == resultY)
-                    {
-                        alert("case déjà colorée")
-                        playerOne = false;
-                        encoche = false;
-                    }
-                });
-                if(encoche == true)
+                for(i = 0; i < 9; i++)
                 {
-                    ctx.fillStyle = "blue";
-                    playerOne = true;
-                    positionBlue = {
-                        posX: resultX,
-                        posY: resultY,
-                        color: "blue"
-                    };
-                    tableauTwo.push(positionBlue);
-                    tableauFinale.push(positionBlue);
-                    decompte.textContent = "Player 1";
-                    console.log(tableauTwo);
-                    ctx.fillRect(resultX, resultY, 199, 199);
-                    tableauTwo.map(function(e){
-                        if(e.posX == 0 && e.posY == 0)
+                    if(tableauPosition[i][0] === resultX && tableauPosition[i][1] === resultY ){
+                        if(tableauResultat[i] == 0)
                         {
-                            tableauTest[0] = tableauTest[0] + 1
-                            tableauTest[3] = tableauTest[3] + 1
-                            tableauTest[5] = tableauTest[5] + 1
+                            let xhr = new XMLHttpRequest;
+                            xhr.open("POST", "http://bigot.cefim.o2switch.site/test", true)
+                            xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+                            xhr.addEventListener("readystatechange", function(e){
+                                console.log(xhr.responseText);
+                            });
+                            xhr.send("resultX=" + resultX + "&resultY=" + resultY + "&color=blue")
+                            tableauResultat[i] = 2
+                            ctx.fillStyle = "blue"
+                            ctx.fillRect(resultX, resultY, 199, 199)
+                            playerOne = true;
+                            for(k = 0; k < 9; k++)
+                            {
+                                tableauTempo[k] = tableauResultat[k];
+                            }
+                            for(a = 0; a < 9; a++)
+                            {
+                                if(tableauResultat[a] == 1)
+                                {
+                                    tableauResultat[a] = 0;
+                                }
+                            }
+                            for(o = 0; o < 8; o++ )
+                            {
+                                for(i = 0; i < 9; i++)
+                                { 
+                                    if(tableauBlue[o][i] === tableauResultat[i])
+                                    {
+                                        gagner++;
+                                        if(gagner == 8)
+                                        {
+                                            console.log("c'est gagné");
+                                        } 
+                                    }
+                                    else
+                                    {
+                                        gagner = 0;
+                                        break;
+                                    }
+                                }
+                            }
+                            tableauBlue.map((e) => {    
+                                for(i = 0; i < 9; i++)
+                                { 
+                                    if(tableauResultat[i] === e)
+                                    {
+                                    }
+                                }
+                            })
+                            for(k = 0; k < 9; k++)
+                            {
+                                tableauResultat[k] = tableauTempo[k]
+                            }
+                            console.log(tableauResultat)
                         }
-                        else if(e.posX == 0 && e.posY == 200)
+                        else
                         {
-                            tableauTest[0] = tableauTest[0] + 1
-                            tableauTest[6] = tableauTest[6] + 1
+                            console.log("case déjà colorié connard")
                         }
-                        else if(e.posX == 0 && e.posY == 400)
-                        {
-                            tableauTest[0] = tableauTest[0] + 1
-                            tableauTest[4] = tableauTest[4] + 1
-                            tableauTest[7] = tableauTest[7] + 1
-                        }
-                        else if(e.posX == 200 && e.posY == 0)
-                        {
-                            tableauTest[1] = tableauTest[1] + 1
-                            tableauTest[5] = tableauTest[5] + 1
-                        }
-                        else if(e.posX == 200 && e.posY == 200)
-                        {
-                            tableauTest[1] = tableauTest[1] + 1
-                            tableauTest[3] = tableauTest[3] + 1
-                            tableauTest[4] = tableauTest[4] + 1
-                            tableauTest[6] = tableauTest[6] + 1
-                        }
-                        else if(e.posX == 200 && e.posY == 400)
-                        {
-                            tableauTest[1] = tableauTest[1] + 1
-                            tableauTest[7] = tableauTest[7] + 1
-                        }
-                        else if(e.posX == 400 && e.posY == 0)
-                        {
-                            tableauTest[2] = tableauTest[2] + 1
-                            tableauTest[4] = tableauTest[4] + 1
-                            tableauTest[5] = tableauTest[5] + 1
-                        }
-                        else if(e.posX == 400 && e.posY == 200)
-                        {
-                            tableauTest[6] = tableauTest[6] + 1
-                            tableauTest[2] = tableauTest[2] + 1
-                        }
-                        else if(e.posX == 400 && e.posY == 400)
-                        {
-                            tableauTest[2] = tableauTest[2] + 1
-                            tableauTest[3] = tableauTest[3] + 1
-                            tableauTest[7] = tableauTest[7] + 1
-                        }
-                    })
+                        break;
+                    }
                 }
-            }
-            console.log(tableauTest);
-            console.log(tableauFinale);
-            console.log(tableauFinale.indexOf(resultX))
-            if(tableauTest[0] == 3 || tableauTest[1] == 3 || tableauTest[2] == 3 || tableauTest[3] == 3 || tableauTest[4] == 3 || tableauTest[5] == 3 || tableauTest[6] == 3 || tableauTest[7] == 3)
-            {
-                alert("Le player 2 a gagné !")
-            }
-            else if(tableauRed[0] == 3 || tableauRed[1] == 3 || tableauRed[2] == 3 || tableauRed[3] == 3 || tableauRed[4] == 3 || tableauRed[5] == 3 || tableauRed[6] == 3 || tableauRed[7] == 3)
-            {
-                alert("Le player 1 a gagné !")
-            }
-            //////////
-            if(tableauFinale.length == 10)
-            {
-                alert("Partie Terminée, pas de gagnant");
-            }
-            
+            }            
     });
 };
+
+setInterval((e) => {            
+    let xhrr = new XMLHttpRequest;
+    xhrr.open("GET", "http://bigot.cefim.o2switch.site/list", true)
+    xhrr.addEventListener("readystatechange", function(e){
+        console.log(xhrr.responseText);
+    });
+    xhrr.send()   
+}, 3000);
 
 
